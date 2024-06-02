149b55d81877e539a43521081238b9004357a971
Dipta Das <diptadassuvo@gmail.com>
2024-06-02 18:25:56 +0600

    feat(router): add signup route to app router
    Enhanced the application routing by adding a new route for the Signup view. This improves navigation within the app.
    
    - Set initial route to `/login`
    - Added `/signup` route pointing to `SignupView`
    - Modified the existing route for login to `/login`
    
    These changes ensure better navigation and prepare the app for user registration functionality.

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,15 +1,21 @@
 import 'package:dokan_app/module/auth/presentation/login_view.dart';
+import 'package:dokan_app/module/auth/presentation/signup_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
+  initialLocation: '/login',
   redirect: (BuildContext context, GoRouterState state) {
     return null;
   },
   routes: [
     GoRoute(
-      path: '/',
+      path: '/login',
       builder: (context, state) => const LoginView(),
+    ),
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => const SignupView(),
     ),
   ],
 );
