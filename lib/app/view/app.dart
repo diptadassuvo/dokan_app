308b147ddd8f000f9350bbebbd5fa0e2e19defd8
Dipta Das <diptadassuvo@gmail.com>
2024-06-02 16:23:54 +0600

    feat: setup screen util for responsive design

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,19 +1,25 @@
 import 'package:dokan_app/app/app_router.dart';
 import 'package:dokan_app/module/auth/presentation/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routerConfig: appRouter,
-    );
+    return ScreenUtilInit(
+        designSize: const Size(430, 932),
+        builder: (_, child) {
+          return MaterialApp.router(
+            title: 'Flutter Demo',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            routerConfig: appRouter,
+          );
+        });
   }
 }
