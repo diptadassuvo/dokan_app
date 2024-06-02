2c6df66cc69fb3f179bbeb65d499969f73bd8d1f
Dipta Das <diptadassuvo@gmail.com>
2024-06-02 17:39:34 +0600

    feat(constants): add assets path constants
    Created `AssetsPath` class to centralize the asset paths used in the application. This improves maintainability and ensures consistent asset referencing.
    
    - Defined constants for app logo, Google logo, and Facebook logo in `core/constants/assets_path.dart`

---
+++ lib/core/constants/assets_path.dart
@@ -0,0 +1,7 @@
+//This class hold the constants Strings of assets path
+
+class AssetsPath {
+  static const String logo = 'assets/images/app_logo.png';
+  static const String googleLogo = 'assets/images/google_logo.png';
+  static const String facebookLogo = 'assets/images/fb_logo.png';
+}
