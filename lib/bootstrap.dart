a7277a05587f6b73e5f4fd594d873dd71aa973a7
Dipta Das <diptadassuvo@gmail.com>
2024-06-03 23:28:47 +0600

    feat: add dependency injection configuration in bootstrap
    - Integrated `configureDependencies()` into `bootstrap.dart` to initialize dependencies before app startup.
    - Ensured dependency injection setup runs before the main UI is built.
    - Updated `bootstrap.dart` formatting for readability.
    
    This change enhances the app's startup process by ensuring all necessary dependencies are configured and available before the UI is displayed.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,18 +1,22 @@
 import 'dart:async';
 
+import 'package:dokan_app/core/config/getit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 /// this function is responsible for bootstrap all utills that needs to initialize
 /// before app main ui start.
 Future<void> bootstrap(
-    FutureOr<Widget> Function() builder,
-    ) async {
+  FutureOr<Widget> Function() builder,
+) async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await SystemChrome.setPreferredOrientations(const <DeviceOrientation>[
     DeviceOrientation.portraitUp,
   ]);
+
+  // inject dependencies
+  configureDependencies();
 
   runApp(await builder());
 }
