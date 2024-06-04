fb5fab9de822f13ce4546015225d99c19aa23e62
Dipta Das <diptadassuvo@gmail.com>
2024-06-04 13:46:17 +0600

    feat: home router add

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,11 +1,12 @@
 import 'package:dokan_app/module/auth/presentation/login/login_view.dart';
 import 'package:dokan_app/module/auth/presentation/signup/signup_view.dart';
-import 'package:dokan_app/module/home/product/presentation/products_view.dart';
+import 'package:dokan_app/module/home/home_view.dart';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/products',
+  initialLocation: '/home',
   redirect: (BuildContext context, GoRouterState state) {
     return null;
   },
@@ -18,7 +19,6 @@ final GoRouter appRouter = GoRouter(
       path: '/signup',
       builder: (context, state) => const SignupView(),
     ),
-    GoRoute(
-        path: '/products', builder: (context, state) => const ProductsView()),
+    GoRoute(path: '/home', builder: (context, state) => const HomeView()),
   ],
 );
