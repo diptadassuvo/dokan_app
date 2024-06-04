cfa6516aab366ad9caf05298b9fbe0f71233b99e
Dipta Das <diptadassuvo@gmail.com>
2024-06-04 13:18:37 +0600

    feat: Add products view to router

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,10 +1,11 @@
 import 'package:dokan_app/module/auth/presentation/login/login_view.dart';
-import 'package:dokan_app/module/auth/presentation/signup_view.dart';
+import 'package:dokan_app/module/auth/presentation/signup/signup_view.dart';
+import 'package:dokan_app/module/home/product/presentation/products_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/products',
   redirect: (BuildContext context, GoRouterState state) {
     return null;
   },
@@ -17,5 +18,7 @@ final GoRouter appRouter = GoRouter(
       path: '/signup',
       builder: (context, state) => const SignupView(),
     ),
+    GoRoute(
+        path: '/products', builder: (context, state) => const ProductsView()),
   ],
 );
