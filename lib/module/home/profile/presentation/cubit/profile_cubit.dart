e18e19e35235e0aaffdd851d37b106b737ef8797
Dipta Das <diptadassuvo@gmail.com>
2024-06-05 22:02:18 +0600

    feat: update profile functionality add to profile cubit

--- lib/module/home/profile/presentation/cubit/profile_cubit.dart
+++ lib/module/home/profile/presentation/cubit/profile_cubit.dart
@@ -3,21 +3,33 @@ import 'package:dokan_app/core/error/failure.dart';
 import 'package:dokan_app/module/home/product/domain/usecases/load_products.dart';
 import 'package:dokan_app/module/home/profile/domain/entities/profile_user_entity.dart';
 import 'package:dokan_app/module/home/profile/domain/usecases/load_profile.dart';
+import 'package:dokan_app/module/home/profile/domain/usecases/update_profile.dart';
 import 'package:meta/meta.dart';
 
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit(this._loadProfileUseCase) : super(ProfileInitial()) {
+  ProfileCubit(this._loadProfileUseCase, this._updateProfileUseCase)
+      : super(ProfileInitial()) {
     loadProfile();
   }
 
   final LoadProfileUseCase _loadProfileUseCase;
 
+  final UpdateProfileUseCase _updateProfileUseCase;
+
   Future<void> loadProfile() async {
     emit(ProfileLoading());
     final response = await _loadProfileUseCase.call();
 
+    response.fold(
+        (error) => emit(ProfileError(
+            error is ServerFailure ? error.message ?? 'error' : '')),
+        (data) => emit(ProfileLoaded(data)));
+  }
+
+  Future<void> updateProfile(UpdateProfileParams params) async {
+    final response = await _updateProfileUseCase.call(params: params);
     response.fold(
         (error) => emit(ProfileError(
             error is ServerFailure ? error.message ?? 'error' : '')),
