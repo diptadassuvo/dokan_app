2fe9fae19d645704d6fb4b665ce62f4a8d2c4692
Dipta Das <diptadassuvo@gmail.com>
2024-06-05 21:59:17 +0600

    fix: profile update param enhanced - make freezed object for tojson method

--- lib/module/home/profile/domain/usecases/update_profile.dart
+++ lib/module/home/profile/domain/usecases/update_profile.dart
@@ -1,19 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:dokan_app/core/error/failure.dart';
 import 'package:dokan_app/module/home/profile/domain/entities/profile_user_entity.dart';
+import 'package:dokan_app/module/home/profile/domain/repos/profile_repo.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'update_profile.freezed.dart';
+
+part 'update_profile.g.dart';
 
 class UpdateProfileUseCase {
-  Future<ProfileUserEntity> call() async {
-    return ProfileUserEntity(
-      userName: 'dokan',
-      firstName: 'dokan',
-      lastName: 'dokan',
-      emailAddress: 'dokan',
-    );
+  final ProfileRepo _profileRepo;
+
+  UpdateProfileUseCase(this._profileRepo);
+
+  Future<Either<Failure, ProfileUserEntity>> call(
+      {required UpdateProfileParams params}) {
+    return _profileRepo.updateProfile(params);
   }
 }
 
-class UpdateProfileParams {
-  final String? firstName;
-  final String? lastName;
+@freezed
+class UpdateProfileParams with _$UpdateProfileParams {
+  const factory UpdateProfileParams({
+    String? first_name,
+    String? last_name,
+  }) = _UpdateProfileParams;
 
-  UpdateProfileParams({required this.firstName, required this.lastName});
+  factory UpdateProfileParams.fromJson(Map<String, dynamic> json) =>
+      _$UpdateProfileParamsFromJson(json);
 }
