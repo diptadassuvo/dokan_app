730d8d55d9e51203e2e695602c59f11a4ecff961
Dipta Das <diptadassuvo@gmail.com>
2024-06-05 19:22:39 +0600

    feat: add profile bloc provider

--- lib/core/config/bloc_providers.dart
+++ lib/core/config/bloc_providers.dart
@@ -5,6 +5,14 @@ import 'package:dokan_app/module/auth/domain/usecases/login.dart';
 import 'package:dokan_app/module/auth/domain/usecases/signup.dart';
 import 'package:dokan_app/module/auth/presentation/login/cubit/login_cubit.dart';
 import 'package:dokan_app/module/auth/presentation/signup/cubit/signup_cubit.dart';
+import 'package:dokan_app/module/home/product/data/datasouces/products_local_datasource_impl.dart';
+import 'package:dokan_app/module/home/product/data/repos/product_repo_impl.dart';
+import 'package:dokan_app/module/home/product/domain/usecases/load_products.dart';
+import 'package:dokan_app/module/home/product/presentation/cubit/products_cubit.dart';
+import 'package:dokan_app/module/home/profile/data/dataSources/profile_remote_datasource_impl.dart';
+import 'package:dokan_app/module/home/profile/data/repos/profile_repo_impl.dart';
+import 'package:dokan_app/module/home/profile/domain/usecases/load_profile.dart';
+import 'package:dokan_app/module/home/profile/presentation/cubit/profile_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
@@ -30,6 +38,30 @@ class BlocProviders {
         SignupUseCase(
           AuthRepoImpl(
             AuthRemoteDatasourceImpl(
+              DioClient(),
+            ),
+          ),
+        ),
+      ),
+    ),
+
+    // product provider
+    BlocProvider<ProductsCubit>(
+      create: (context) => ProductsCubit(
+        LoadProductsUsecase(
+          ProductRepoImpl(
+            ProductsLocalDatasourceImpl(),
+          ),
+        ),
+      ),
+    ),
+
+    // profile provider
+    BlocProvider<ProfileCubit>(
+      create: (context) => ProfileCubit(
+        LoadProfileUseCase(
+          ProfileRepoImpl(
+            ProfileRemoteDatasourceImpl(
               DioClient(),
             ),
           ),
