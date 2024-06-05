929ca72e4ebf12ec455e97bf154f22d6b76bd35a
Dipta Das <diptadassuvo@gmail.com>
2024-06-05 22:27:02 +0600

    fix: App theme enhanced

--- lib/core/config/theme.dart
+++ lib/core/config/theme.dart
@@ -2,39 +2,41 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final baseTheme = ThemeData(
-    useMaterial3: true,
+      useMaterial3: true,
+      colorSchemeSeed: const Color(0xFFF75F55),
 
-    colorSchemeSeed: const Color(0xFFF75F55),
+      // scaffold theme
+      scaffoldBackgroundColor: const Color(0xFFf8f8f8),
 
-    // scaffold theme
-    scaffoldBackgroundColor: const Color(0xFFf8f8f8),
-
-    // Text input theme
-    inputDecorationTheme: const InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        borderSide: BorderSide(color: Colors.transparent),
+      // Text input theme
+      inputDecorationTheme: const InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(color: Colors.transparent),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(color: Color(0x1F263238)),
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        prefixIconColor: Color(0xff7C8592),
+        suffixIconColor: Color(0xff7C8592),
       ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        borderSide: BorderSide(color: Color(0x1F263238)),
-      ),
-      fillColor: Colors.white,
-      filled: true,
-      prefixIconColor: Color(0xff7C8592),
-      suffixIconColor: Color(0xff7C8592),
-    ),
 
-    // button theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        foregroundColor: WidgetStateProperty.all(Colors.white),
-        shape: WidgetStateProperty.all(
-          const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      // button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: WidgetStateProperty.all(Colors.white),
+          shape: WidgetStateProperty.all(
+            const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
           ),
         ),
       ),
-    ),
-  );
+      checkboxTheme: CheckboxThemeData(
+        checkColor: WidgetStateProperty.all(Colors.white),
+        side: const BorderSide(width: 2, color: Color(0xffFF708A)),
+      ));
 }
