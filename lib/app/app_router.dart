23c335fe3a4fa2bf299972a525c47573f3e67fac
Dipta Das <diptadassuvo@gmail.com>
2024-06-05 19:23:09 +0600

    feat: enhanced router redirect

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:dokan_app/module/auth/presentation/login/login_view.dart';
 import 'package:dokan_app/module/auth/presentation/signup/signup_view.dart';
 import 'package:dokan_app/module/home/home_view.dart';
+import 'package:dokan_app/utils/services/hive/main_box.dart';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -8,6 +9,11 @@ import 'package:go_router/go_router.dart';
 final GoRouter appRouter = GoRouter(
   initialLocation: '/home',
   redirect: (BuildContext context, GoRouterState state) {
+    final bool isLoggedIn = MainBoxMixin().getData(MainBoxKeys.token) != null;
+
+    if (state.uri.toString() == '/home' && !isLoggedIn) {
+      return '/login';
+    }
     return null;
   },
   routes: [
